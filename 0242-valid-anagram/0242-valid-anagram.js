bd62a5ddc5556c18b8063352633b89687ee753e6
Esraa <96691689+Esraa9955@users.noreply.github.com>
2024-05-31 22:10:31 +0200

    Time: 72 ms (55.32%), Space: 55.2 MB (10.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length!==t.length){
+        
+        return false
+    }
+    const countS={};
+    const countT={};
+    for(let char of s){
+        countS[char]=(countS[char]||0)+1;
+    }
+     for(let char of t){
+        countT[char]=(countT[char]||0)+1;
+    }
+for(let char in countS){
+    if(countS[char]!==countT[char]){
+       return false
+       }}
+return true
+};
