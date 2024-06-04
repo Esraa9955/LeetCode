55e65d8b935346c72b8889fd71e06a93d5bf388c
Esraa <96691689+Esraa9955@users.noreply.github.com>
2024-06-04 22:47:37 +0200

    Time: 51 ms (59.07%), Space: 48.8 MB (59.74%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    s=s.trim();
+    const words=s.split(' ');
+    for(let i=words.length-1;i>=0;i--){
+        if(words[i]){
+           return words[i].length;
+           }
+    }
+    return 0;
+};
